68e434789fa334d7ee5c8f1c72d5c02587812ea2
mania605 <kim.minsun83@gmail.com>
2024-09-20 14:27:33 +0900

    이벤트 발생시 event.target으로 부터 속성 정보 가져오기

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+/*
+  forEach
+  : 배열, 유사배열을 반복처리하기 위한 내장 함수
+  Array.forEach((data, idx)=>{
+    중괄호 안쪽의 코드블록은 배열의 갯수만큼 반복 처리됨
+    해당 코드블록 안쪽에서는 data, idx라는 파라미터로 전달되는 값 활용 가능
+    data: 현재 반복도는 배열의 데이터
+    idx: 현재 반복도는 순서값
+  })
+*/
 const btns = document.querySelectorAll("button");
-const posArr = [0, 1000, 2000, 3000, 4000]; // 스크롤할 위치 배열
 
-btns.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
-    new Anime(window, { scroll: posArr[index] }, { duration: 500 });
+btns.forEach((btn, idx) => {
+  //forEach반복문 안쪽에서 반복도는 각 btn요소에 click이벤트 연결
+  btn.addEventListener("click", (event) => {
+    //각 반복도는 btn요소 클릭시 자동으로 전달되는 event.target
+    //클릭이라는 이벹트가 발생한 바로그 DOM요소를 지칭
+    console.log(event.target);
+    //이벤트가 발생한 해당 DOM요소에 지정된 data-pos라는 정보값 가져옴
+    console.log(event.target.getAttribute("data-pos"));
   });
 });
 
